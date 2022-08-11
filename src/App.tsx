e7baaee7e24207169d4d2add0792b8d96b20ac99
gkseogus <fbznffldj998@naver.com>
2022-08-11 18:50:48 +0900

    220811_build

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import AllRoutesApp from './allRoutes';
 import { ChakraProvider } from '@chakra-ui/react'
+import { BrowserRouter as Router } from 'react-router-dom';
 
 const App = () => {
   return (
     <div className="App">
-      <ChakraProvider>
-        <AllRoutesApp/>
-      </ChakraProvider>
+      <Router basename={'process.env.PUBLIC_URL'}>
+        <ChakraProvider>
+          <AllRoutesApp/>
+        </ChakraProvider>
+      </Router>
     </div>
   );
 }
