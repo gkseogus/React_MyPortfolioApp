fdd573a2b043de8c5b5e452849941848fc2e3d4a
gkseogus <fbznffldj998@naver.com>
2022-08-17 18:22:24 +0900

    220817_개발자도구 콘솔창 메세지

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,22 @@
 import AllRoutesApp from './routesAll';
 import { ChakraProvider } from '@chakra-ui/react'
 import MainNavBar from './components/MainNavBar';
+import Footer from './components/Footer';
+
+console.log(`%c
+██╗  ██╗███████╗██╗     ██╗      ██████╗ 
+██║  ██║██╔════╝██║     ██║     ██╔═══██╗
+███████║█████╗  ██║     ██║     ██║   ██║
+██╔══██║██╔══╝  ██║     ██║     ██║   ██║
+██║  ██║███████╗███████╗███████╗╚██████╔╝
+╚═╝  ╚═╝╚══════╝╚══════╝╚══════╝ ╚═════╝ 
+██╗  ██╗██████╗ ██╗  ██╗    ██████╗ ██╗      ██████╗  ██████╗ 
+██║  ██║██╔══██╗██║  ██║    ██╔══██╗██║     ██╔═══██╗██╔════╝ 
+███████║██║  ██║███████║    ██████╔╝██║     ██║   ██║██║  ███╗
+██╔══██║██║  ██║██╔══██║    ██╔══██╗██║     ██║   ██║██║   ██║
+██║  ██║██████╔╝██║  ██║    ██████╔╝███████╗╚██████╔╝╚██████╔╝
+╚═╝  ╚═╝╚═════╝ ╚═╝  ╚═╝    ╚═════╝ ╚══════╝ ╚═════╝  ╚═════╝ 
+`,'background:white; color:blue;');
 
 const App = () => {
   return (
@@ -8,6 +24,7 @@ const App = () => {
       <ChakraProvider>
         <MainNavBar />
         <AllRoutesApp />
+        <Footer />
       </ChakraProvider>
     </div>
   );
