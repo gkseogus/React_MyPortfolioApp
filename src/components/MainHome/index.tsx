0ae49d1067eea5499bc1ab69682696f9434c0991
gkseogus <fbznffldj998@naver.com>
2022-08-18 15:58:01 +0900

    220818_add to main page css animation

--- src/components/MainHome/index.tsx
+++ src/components/MainHome/index.tsx
@@ -4,11 +4,41 @@ import {
     Hide,
     Box
   } from '@chakra-ui/react';
-import styled from 'styled-components';
+import styled, {keyframes} from 'styled-components';
 import FirstIntroImg from '../MainHome/IMG/FirstIntroImg.png';
 import GitHubSocialCard from '../MainHome/GitHubSocialCard/index';
 import ProjectCaptionCarousel from './ProjectCarousel';
 import IntroCareers from './CareersCard';
+
+const circleAni = keyframes`
+  0%{
+    transform: rotate(0deg);
+  }
+  50%{
+    transform: rotate(180deg);
+  }
+  100%{
+    transform: rotate(360deg);
+  }
+`;
+
+const gradientIntro = keyframes`
+	0%{
+        color: white;
+    }
+    100%{
+        color: black;
+    }
+`
+
+const gradientContents = keyframes`
+    from {
+        -webkit-filter: hue-rotate(0deg);
+    }
+    to {
+        -webkit-filter: hue-rotate(-360deg);
+    }
+`
 
 const IntroContain = styled.div`
 
@@ -28,21 +58,118 @@ const IntroText = styled.h2`
     margin-inline-start: 10%;
     margin-inline-end: 10%;
     display: flex;
+    animation: ${gradientIntro} 6s ease-in-out none;
 `
 
 const IntroImg = styled.img`
     position: absolute;
-    left: 65%;
-    top: 45%;
+    bottom: 5%;
+    right: 5%;
     transform: translate(-50%, -50%);
     width: 40%;
     height: 35%;
     min-height: 500px;
-    min-width: 1000px;
+    min-width: 700px;
     opacity: 0.4;
+    animation: ${circleAni} 10s  linear infinite;
 `
 
-const ContentsText = styled.h2`
+const Openclose = keyframes`
+0% {
+    top: 0.2rem;
+    width: 0;
+  }
+  5% {
+    width: 0;
+  }
+  15% {
+    width: 50%;
+  }
+  30% {
+    top: 0.2rem;
+    width: 50%;
+  }
+  33% {
+    top: 0.2rem;
+    width: 0;
+  }
+  35% {
+    top: 0.2rem;
+    width: 0;
+  }
+  38% {
+    top: -4.5rem;
+    width: 0;
+  }
+  48% {
+    top: -4.5rem;
+    width: 50%;
+  }
+  62% {
+    top: -4.5rem;
+    width: 50%;
+  }
+  66% {
+    top: -4.5rem;
+    width: 0;
+    text-indent: 0;
+  }
+  71% {
+    top: -9rem;
+    width: 0;
+    text-indent: 5px;
+  }
+  86% {
+    top: -9rem;
+    width: 50%;
+  }
+  95% {
+    top: -9rem;
+    width: 50%;
+  }
+  98% {
+    top: -9rem;
+    width: 0;
+    text-indent: 5px;
+  }
+  100% {
+    top: 0;
+    width: 0;
+    text-indent: 0;
+  }
+}
+`
+
+const FirstContentsText = styled.h1`
+    color: black;
+    font-family: tahoma;
+    font-size: 3rem;
+    font-weight: 100;
+    line-height: 1.5;
+    text-transform: uppercase;
+    white-space: nowrap;
+    overflow: hidden;
+    position: relative;
+    width: 550px;
+    left: 10%;
+    margin: 25%;
+`
+
+const Message = styled.div`
+    background-color: red;
+    color: black;
+    display: block;
+    font-weight: 900;
+    font-family: 'Kanit', sans-serif;
+    overflow: hidden;
+    position: absolute;
+    padding-left: 0.5rem;
+    top: 0.2rem;
+    left: 200px;
+    animation: ${Openclose} 5s ease-in-out infinite;
+`
+
+const SecondContentsText = styled.h2`
     position: relative;
     text-align: left;
     font-size: 64px;
@@ -54,6 +181,11 @@ const ContentsText = styled.h2`
     margin-inline-start: 10%;
     margin-inline-end: 10%;
     display: flex;
+    background-image: -webkit-linear-gradient(92deg, #eb5757, #000000);
+    -webkit-background-clip: text;
+    -webkit-text-fill-color: transparent;
+    -webkit-animation: hue 10s infinite linear;
+    animation: ${gradientContents} 4s ease-in-out infinite;
 `
 
 const GitHubLinkContain = styled.div`
@@ -72,6 +204,11 @@ const ReverseContentsText = styled.h2`
     margin-inline-start: 68%;
     margin-inline-end: 10%;
     display: flex;
+    background-image: -webkit-linear-gradient(92deg, #eb5757, #000000);
+    -webkit-background-clip: text;
+    -webkit-text-fill-color: transparent;
+    -webkit-animation: hue 10s infinite linear;
+    animation: ${gradientContents} 4s ease-in-out infinite;
 `
 
 const ReverseContentsBtn = styled(Link)`
@@ -105,6 +242,11 @@ const CenterContentsText = styled.h2`
     line-height: 1.27;
     letter-spacing: 0px;
     padding: 10px;
+    background-image: -webkit-linear-gradient(92deg, #eb5757, #000000);
+    -webkit-background-clip: text;
+    -webkit-text-fill-color: transparent;
+    -webkit-animation: hue 10s infinite linear;
+    animation: ${gradientContents} 4s ease-in-out infinite;
 `
 
 const IntroCareersContain = styled.div`
@@ -117,7 +259,19 @@ const IntroCareersContain = styled.div`
     padding-bottom: 30%;
 `
 
+// scroll custom hook
+// export const useWindowScrollEvent = (listener: (this: Window, ev: Event) => any) => {
+//     useEffect(() => {
+//         window.addEventListener('scroll', listener);
+
+//         return () => {
+//         window.removeEventListener('scroll', listener);
+//         };
+//     }, []);
+// };
+
 const MainHome = () => {
+
     return (
         <IntroContain>
             <div>
@@ -132,8 +286,16 @@ const MainHome = () => {
                     </Hide>
                 </IntroText>
             </div>
+            <FirstContentsText>
+                <span>My own</span>
+                <Message>
+                    <div>thinking</div>
+                    <div>planning</div>
+                    <div>creating</div>
+                </Message>
+            </FirstContentsText>
             <div>
-                <ContentsText>My Git Hub</ContentsText>
+                <SecondContentsText>My Git Hub</SecondContentsText>
                 <GitHubLinkContain>
                     <GitHubSocialCard />
                 </GitHubLinkContain>
