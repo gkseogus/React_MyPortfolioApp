d99ede35e3bef1c03996b2a4ff5f97cb7db6d08c
gkseogus <fbznffldj998@naver.com>
2022-08-18 16:31:00 +0900

    220818_delete css animation

--- src/components/MainHome/index.tsx
+++ src/components/MainHome/index.tsx
@@ -74,102 +74,7 @@ const IntroImg = styled.img`
     animation: ${circleAni} 10s  linear infinite;
 `
 
-const Openclose = keyframes`
-0% {
-    top: 0.2rem;
-    width: 0;
-  }
-  5% {
-    width: 0;
-  }
-  15% {
-    width: 50%;
-  }
-  30% {
-    top: 0.2rem;
-    width: 50%;
-  }
-  33% {
-    top: 0.2rem;
-    width: 0;
-  }
-  35% {
-    top: 0.2rem;
-    width: 0;
-  }
-  38% {
-    top: -4.5rem;
-    width: 0;
-  }
-  48% {
-    top: -4.5rem;
-    width: 50%;
-  }
-  62% {
-    top: -4.5rem;
-    width: 50%;
-  }
-  66% {
-    top: -4.5rem;
-    width: 0;
-    text-indent: 0;
-  }
-  71% {
-    top: -9rem;
-    width: 0;
-    text-indent: 5px;
-  }
-  86% {
-    top: -9rem;
-    width: 50%;
-  }
-  95% {
-    top: -9rem;
-    width: 50%;
-  }
-  98% {
-    top: -9rem;
-    width: 0;
-    text-indent: 5px;
-  }
-  100% {
-    top: 0;
-    width: 0;
-    text-indent: 0;
-  }
-}
-`
-
-const FirstContentsText = styled.h1`
-    color: black;
-    font-family: tahoma;
-    font-size: 3rem;
-    font-weight: 100;
-    line-height: 1.5;
-    text-transform: uppercase;
-    white-space: nowrap;
-    overflow: hidden;
-    position: relative;
-    width: 550px;
-    left: 10%;
-    margin: 25%;
-`
-
-const Message = styled.div`
-    background-color: red;
-    color: black;
-    display: block;
-    font-weight: 900;
-    font-family: 'Kanit', sans-serif;
-    overflow: hidden;
-    position: absolute;
-    padding-left: 0.5rem;
-    top: 0.2rem;
-    left: 200px;
-    animation: ${Openclose} 5s ease-in-out infinite;
-`
-
-const SecondContentsText = styled.h2`
+const ContentsText = styled.h2`
     position: relative;
     text-align: left;
     font-size: 64px;
@@ -286,23 +191,15 @@ const MainHome = () => {
                     </Hide>
                 </IntroText>
             </div>
-            <FirstContentsText>
-                <span>My own</span>
-                <Message>
-                    <div>thinking</div>
-                    <div>planning</div>
-                    <div>creating</div>
-                </Message>
-            </FirstContentsText>
             <div>
-                <SecondContentsText>My Git Hub</SecondContentsText>
+                <ContentsText>My Git Hub</ContentsText>
                 <GitHubLinkContain>
                     <GitHubSocialCard />
                 </GitHubLinkContain>
             </div>
             <div>
                 <ReverseContentsText>My Project</ReverseContentsText>
-                <ReverseContentsBtn to={'/projects'}>Project</ReverseContentsBtn>
+                <ReverseContentsBtn to={'/Project'}>Project</ReverseContentsBtn>
                 <ProjectCarouselsContain>
                     <ProjectCaptionCarousel/>
                 </ProjectCarouselsContain>
