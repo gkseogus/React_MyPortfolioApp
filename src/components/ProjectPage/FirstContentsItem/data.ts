ab0eb95981d292430b7a6448501ea0f7c5f7b0a0
gkseogus <fbznffldj998@naver.com>
2022-08-19 18:24:15 +0900

    220819_FirstContentsItem data

---
+++ src/components/ProjectPage/FirstContentsItem/data.ts
@@ -0,0 +1,34 @@
+import te from "./IMG/commuteImg.png";
+
+const data = [
+  {
+    name: "Rare Wind",
+    description: "#a8edea → #fed6e3",
+    css: "linear-gradient(135deg, #a8edea 0%, #fed6e3 100%)",
+    height: 200,
+    langthImg: te,
+  },
+  {
+    name: "Saint Petersburg",
+    description: "#f5f7fa → #c3cfe2",
+    css: "linear-gradient(135deg, #f5f7fa 0%, #c3cfe2 100%)",
+    height: 400,
+    langthImg: "ttt",
+  },
+  {
+    name: "Saint Petersburg",
+    description: "#f5f7fa → #c3cfe2",
+    css: "linear-gradient(135deg, #f5f7fa 0%, #c3cfe2 100%)",
+    height: 400,
+    langthImg: "ttt",
+  },
+  {
+    name: "Saint Petersburg",
+    description: "#f5f7fa → #c3cfe2",
+    css: "linear-gradient(135deg, #f5f7fa 0%, #c3cfe2 100%)",
+    height: 400,
+    langthImg: "ttt",
+  },
+];
+
+export default data;
