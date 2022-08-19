75769df11f6535aa5a705ddfd27f07378e277a5d
gkseogus <fbznffldj998@naver.com>
2022-08-19 11:12:45 +0900

    220819_color mode 버튼 추가

--- src/components/MainNavBar/index.tsx
+++ src/components/MainNavBar/index.tsx
@@ -1,4 +1,4 @@
-import { ReactNode } from 'react';
+import { ReactNode } from "react";
 import {
   Box,
   Flex,
@@ -7,6 +7,8 @@ import {
   Link,
   IconButton,
   Button,
+  ButtonProps,
+  useColorMode,
   Menu,
   MenuButton,
   MenuList,
@@ -15,119 +17,144 @@ import {
   useDisclosure,
   useColorModeValue,
   Stack,
-} from '@chakra-ui/react';
-import { HamburgerIcon, CloseIcon } from '@chakra-ui/icons';
-import styled from 'styled-components';
+} from "@chakra-ui/react";
+import { HamburgerIcon, CloseIcon } from "@chakra-ui/icons";
+import { BsSun, BsMoonStarsFill } from "react-icons/bs";
+import styled from "styled-components";
 
-import HdhLogo from '../MainNavBar/IMG/mainLogo.png';
-import AvatarImg from '../MainNavBar/IMG/AvatarLogo.png';
-import InstartgramLogo from '../MainNavBar/IMG/InstargramLogo.png';
-import CareerlyLogo from '../MainNavBar/IMG/CareerlyLogo.png';
-import GithubLogo from '../MainNavBar/IMG/GithubLogo.png';
+import HdhLogo from "../MainNavBar/IMG/mainLogo.png";
+import AvatarImg from "../MainNavBar/IMG/AvatarLogo.png";
+import InstartgramLogo from "../MainNavBar/IMG/InstargramLogo.png";
+import CareerlyLogo from "../MainNavBar/IMG/CareerlyLogo.png";
+import GithubLogo from "../MainNavBar/IMG/GithubLogo.png";
 
 const NavBarLogo = styled.img`
-    width: 200px;
-    height: 150px;
-    margin-top: 20px;
-    margin-left: 20px;
-`
+  width: 200px;
+  height: 150px;
+  margin-top: 20px;
+  margin-left: 20px;
+`;
 
 const AvatarMenuListLogo = styled.img`
-    width: 20px;
-    height: 20px;
-    margin: 20px;
-`
+  width: 20px;
+  height: 20px;
+  margin: 20px;
+`;
 
-const Links = ['Home', 'Project', 'Career'];
+const Links = ["Home", "Project", "Career"];
 
-const NavLink = ({ children, href }: { children: ReactNode, href: string }) => (
+const NavLink = ({ children, href }: { children: ReactNode; href: string }) => (
   <Link
     px={2}
     py={1}
-    rounded={'md'}
+    rounded={"md"}
     _hover={{
-      textDecoration: 'none',
-      background: 'black',
-      color :'red'
+      textDecoration: "none",
+      background: "black",
+      color: "red",
     }}
-    href={href}>
+    href={href}
+  >
     {children}
   </Link>
 );
+
+const ColorModeToggle = (props: ButtonProps) => {
+  const { colorMode, toggleColorMode } = useColorMode();
+  return (
+    <Flex h="100vh" justifyContent="center" alignItems="center">
+      <Button
+        aria-label="Toggle Color Mode"
+        onClick={toggleColorMode}
+        _focus={{ boxShadow: "none" }}
+        w="fit-content"
+        {...props}
+      >
+        {colorMode === "light" ? <BsMoonStarsFill /> : <BsSun />}
+      </Button>
+    </Flex>
+  );
+};
 
 export default function MainNavBar(_children: any) {
   const { isOpen, onOpen, onClose } = useDisclosure();
 
   return (
     <div>
-      <Box bg={useColorModeValue('black', 'black')} px={4} fontSize={'20px'} fontWeight={600}>
-        <Flex h={32} alignItems={'center'} justifyContent={'space-between'}>
+      <Box
+        bg={useColorModeValue("black", "black")}
+        px={4}
+        fontSize={"20px"}
+        fontWeight={600}
+      >
+        <Flex h={32} alignItems={"center"} justifyContent={"space-between"}>
           <IconButton
-            size={'md'}
+            size={"md"}
             icon={isOpen ? <CloseIcon /> : <HamburgerIcon />}
-            aria-label={'Open Menu'}
-            display={{ md: 'none' }}
+            aria-label={"Open Menu"}
+            display={{ md: "none" }}
             onClick={isOpen ? onClose : onOpen}
           />
-          <HStack spacing={8} alignItems={'center'}>
-            <NavBarLogo src= {HdhLogo} />
+          <HStack spacing={8} alignItems={"center"}>
+            <NavBarLogo src={HdhLogo} />
             <HStack
-              as={'nav'}
+              as={"nav"}
               spacing={4}
-              display={{ base: 'none', md: 'flex' }}
-              color="white">
+              display={{ base: "none", md: "flex" }}
+              color="white"
+            >
               {Links.map((link) => (
-                <NavLink key={link} href={link}>{link}</NavLink>
+                <NavLink key={link} href={link}>
+                  {link}
+                </NavLink>
               ))}
             </HStack>
           </HStack>
-          <Flex alignItems={'center'}>
+          <Flex alignItems={"center"}>
+            <ColorModeToggle />
             <Menu>
               <MenuButton
                 as={Button}
-                rounded={'full'}
-                variant={'link'}
-                cursor={'pointer'}
-                minW={0}>
-                <Avatar
-                  size={'md'}
-                  src={AvatarImg}
-                  bg='black'
-                />
+                rounded={"full"}
+                variant={"link"}
+                cursor={"pointer"}
+                minW={0}
+              >
+                <Avatar size={"md"} src={AvatarImg} bg="black" />
               </MenuButton>
-              <MenuList color='black'>
-                <MenuItem 
-                _hover={{
-                  background: 'white',
-                  color :'red'
-                }}   
-                as="a"
-                href="https://www.instagram.com/dae_hyeon00/"
+              <MenuList color="black">
+                <MenuItem
+                  _hover={{
+                    background: "white",
+                    color: "red",
+                  }}
+                  as="a"
+                  href="https://www.instagram.com/dae_hyeon00/"
                 >
-                  <AvatarMenuListLogo src= {InstartgramLogo} />
+                  <AvatarMenuListLogo src={InstartgramLogo} />
                   Instargram
                 </MenuItem>
-                <MenuItem 
-                _hover={{
-                  background: 'white',
-                  color :'red'
-                }}   
-                as="a"
-                href="https://careerly.co.kr/profiles/496283?from=newsfeed&location=gnb"
+                <MenuItem
+                  _hover={{
+                    background: "white",
+                    color: "red",
+                  }}
+                  as="a"
+                  href="https://careerly.co.kr/profiles/496283?from=newsfeed&location=gnb"
                 >
-                  <AvatarMenuListLogo src= {CareerlyLogo} />
+                  <AvatarMenuListLogo src={CareerlyLogo} />
                   Careerly
                 </MenuItem>
                 <MenuDivider />
-                <MenuItem 
-                _hover={{
-                  background: 'white',
-                  color :'red'
-                }}   
-                as="a"
-                href="https://github.com/gkseogus"
+                <MenuItem
+                  _hover={{
+                    background: "white",
+                    color: "red",
+                  }}
+                  as="a"
+                  href="https://github.com/gkseogus"
                 >
-                  <AvatarMenuListLogo src= {GithubLogo} />
+                  <AvatarMenuListLogo src={GithubLogo} />
                   Github
                 </MenuItem>
               </MenuList>
@@ -135,10 +162,12 @@ export default function MainNavBar(_children: any) {
           </Flex>
         </Flex>
         {isOpen ? (
-          <Box pb={4} display={{ md: 'none' }} >
-            <Stack as={'nav'} spacing={4}>
+          <Box pb={4} display={{ md: "none" }}>
+            <Stack as={"nav"} spacing={4}>
               {Links.map((link) => (
-                <NavLink key={link} href={link}>{link}</NavLink>
+                <NavLink key={link} href={link}>
+                  {link}
+                </NavLink>
               ))}
             </Stack>
           </Box>
