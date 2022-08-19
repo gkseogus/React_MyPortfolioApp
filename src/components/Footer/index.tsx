72a17da4a878fcaa8f1ec16ddbf861379afe0dae
gkseogus <fbznffldj998@naver.com>
2022-08-19 18:47:26 +0900

    220819_코드정리

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,121 +1,179 @@
 import {
-    Box,
-    Container,
-    Link,
-    SimpleGrid,
-    Stack,
-    Text,
-    Flex,
-    Tag,
-    useColorModeValue,
-} from '@chakra-ui/react';
-import { ReactNode } from 'react';
-import styled from 'styled-components';
-import FooterLogo from '../Footer/footerLogo.png';
+  Box,
+  Container,
+  Link,
+  SimpleGrid,
+  Stack,
+  Text,
+  Flex,
+  Tag,
+  useColorModeValue,
+} from "@chakra-ui/react";
+import { ReactNode } from "react";
+import styled from "styled-components";
+import FooterLogo from "../Footer/footerLogo.png";
 
 const LogoContain = styled.div`
-    width: 130px;
-    text-align: center;
-`
+  width: 130px;
+  text-align: center;
+`;
 const Logo = (props: any) => {
-    return (
-        <LogoContain>
-            <img alt='footer logo' src={FooterLogo}/>
-        </LogoContain>
-    );
+  return (
+    <LogoContain>
+      <img alt="footer logo" src={FooterLogo} />
+    </LogoContain>
+  );
 };
 
 const ListHeader = ({ children }: { children: ReactNode }) => {
-
-    return (
-        <Text fontWeight={'500'} fontSize={'lg'} mb={2}>
-        {children}
-        </Text>
-    );
+  return (
+    <Text fontWeight={"500"} fontSize={"lg"} mb={2}>
+      {children}
+    </Text>
+  );
 };
 
 const Footer = () => {
+  const getDomain = (domain: string) => {
+    window.open(domain);
+  };
 
-    const getDomain = (domain: string) => {
-        window.open(domain);
-    };
-
-    return (
-        <Box
-        bg={useColorModeValue('gray.50', 'gray.900')}
-        color={useColorModeValue('gray.700', 'gray.200')}>
-        <Container as={Stack} maxW={'6xl'} py={10}>
-            <SimpleGrid columns={{ base: 1, sm: 2, md: 5 }} spacing={5} style={{textAlign: 'center'}}>
-            <Stack align={'flex-start'}>
-                <ListHeader>Product</ListHeader>
-                <Link href={'Home'}>Home</Link>
-                <Stack direction={'row'} align={'center'} spacing={2}>
-                <Link href={'Project'}>Project</Link>
-                <Tag
-                    size={'sm'}
-                    bg={useColorModeValue('black', 'black')}
-                    ml={2}
-                    color={'white'}>
-                    New
-                </Tag>
-                </Stack>
-                <Stack direction={'row'} align={'center'} spacing={2}>
-                <Link href={'Career'}>Career</Link>
-                <Tag
-                    size={'sm'}
-                    bg={useColorModeValue('black', 'black')}
-                    ml={2}
-                    color={'white'}>
-                    New
-                </Tag>
-                </Stack>
+  return (
+    <Box
+      bg={useColorModeValue("gray.50", "gray.900")}
+      color={useColorModeValue("gray.700", "gray.200")}
+    >
+      <Container as={Stack} maxW={"6xl"} py={10}>
+        <SimpleGrid
+          columns={{ base: 1, sm: 2, md: 5 }}
+          spacing={5}
+          style={{ textAlign: "center" }}
+        >
+          <Stack align={"flex-start"}>
+            <ListHeader>Product</ListHeader>
+            <Link href={"Home"}>Home</Link>
+            <Stack direction={"row"} align={"center"} spacing={2}>
+              <Link href={"Project"}>Project</Link>
+              <Tag
+                size={"sm"}
+                bg={useColorModeValue("black", "black")}
+                ml={2}
+                color={"white"}
+              >
+                New
+              </Tag>
             </Stack>
-            <Stack align={'flex-start'}>
-                <ListHeader>Company</ListHeader>
-                <Link onClick={()=>{getDomain('https://www.hnine.com/')}}>Hnine</Link>
-                <Link onClick={()=>{getDomain('https://jinjin.co.kr/main.do')}}>JinJin System</Link>
+            <Stack direction={"row"} align={"center"} spacing={2}>
+              <Link href={"Career"}>Career</Link>
+              <Tag
+                size={"sm"}
+                bg={useColorModeValue("black", "black")}
+                ml={2}
+                color={"white"}
+              >
+                New
+              </Tag>
             </Stack>
-            <Stack align={'flex-start'}>
-                <ListHeader>Job</ListHeader>
-                <Link onClick={()=>{getDomain('https://www.jobkorea.co.kr/')}}>Jop Korea</Link>
-                <Link onClick={()=>{getDomain('https://www.saramin.co.kr/zf_user/')}}>Saramin</Link>
-                <Link onClick={()=>{getDomain('https://www.wanted.co.kr/')}}>Wanted</Link>
-            </Stack>
-            <Stack align={'flex-start'}>
-                <ListHeader>Follow Us</ListHeader>
-                <Link onClick={()=>{getDomain('https://www.linkedin.com/in/dae-hyeon-han-468005249/')}}>LinkedIn</Link>
-                <Link onClick={()=>{getDomain('https://careerly.co.kr/profiles/496283?from=%2Fmessenger&location=gnb/')}}>Careerly</Link>
-            </Stack>
-            <Stack align={'flex-start'}>
-                <ListHeader>Univ</ListHeader>
-                <Link onClick={()=>{getDomain('https://www.hallym.ac.kr/hallym_univ/')}}>Hallym University</Link>
-            </Stack>
-            </SimpleGrid>
-        </Container>
-        <Box py={10}>
-            <Flex
-            align={'center'}
-            _before={{
-                content: '""',
-                borderBottom: '1px solid',
-                borderColor: useColorModeValue('gray.200', 'gray.700'),
-                flexGrow: 1,
-                mr: 8,
-            }}
-            _after={{
-                content: '""',
-                borderBottom: '1px solid',
-                borderColor: useColorModeValue('gray.200', 'gray.700'),
-                flexGrow: 1,
-                ml: 8,
-            }}>
-            <Logo />
-            </Flex>
-            <Text pt={0} fontSize={'sm'} textAlign={'center'}>
-            © 2022.8.11 부터 구축된 블로그입니다.
-            </Text>
-        </Box>
-        </Box>
-    );
-}
+          </Stack>
+          <Stack align={"flex-start"}>
+            <ListHeader>Company</ListHeader>
+            <Link
+              onClick={() => {
+                getDomain("https://www.hnine.com/");
+              }}
+            >
+              Hnine
+            </Link>
+            <Link
+              onClick={() => {
+                getDomain("https://jinjin.co.kr/main.do");
+              }}
+            >
+              JinJin System
+            </Link>
+          </Stack>
+          <Stack align={"flex-start"}>
+            <ListHeader>Job</ListHeader>
+            <Link
+              onClick={() => {
+                getDomain("https://www.jobkorea.co.kr/");
+              }}
+            >
+              Jop Korea
+            </Link>
+            <Link
+              onClick={() => {
+                getDomain("https://www.saramin.co.kr/zf_user/");
+              }}
+            >
+              Saramin
+            </Link>
+            <Link
+              onClick={() => {
+                getDomain("https://www.wanted.co.kr/");
+              }}
+            >
+              Wanted
+            </Link>
+          </Stack>
+          <Stack align={"flex-start"}>
+            <ListHeader>Follow Us</ListHeader>
+            <Link
+              onClick={() => {
+                getDomain(
+                  "https://www.linkedin.com/in/dae-hyeon-han-468005249/"
+                );
+              }}
+            >
+              LinkedIn
+            </Link>
+            <Link
+              onClick={() => {
+                getDomain(
+                  "https://careerly.co.kr/profiles/496283?from=%2Fmessenger&location=gnb/"
+                );
+              }}
+            >
+              Careerly
+            </Link>
+          </Stack>
+          <Stack align={"flex-start"}>
+            <ListHeader>Univ</ListHeader>
+            <Link
+              onClick={() => {
+                getDomain("https://www.hallym.ac.kr/hallym_univ/");
+              }}
+            >
+              Hallym University
+            </Link>
+          </Stack>
+        </SimpleGrid>
+      </Container>
+      <Box py={10}>
+        <Flex
+          align={"center"}
+          _before={{
+            content: '""',
+            borderBottom: "1px solid",
+            borderColor: useColorModeValue("gray.200", "gray.700"),
+            flexGrow: 1,
+            mr: 8,
+          }}
+          _after={{
+            content: '""',
+            borderBottom: "1px solid",
+            borderColor: useColorModeValue("gray.200", "gray.700"),
+            flexGrow: 1,
+            ml: 8,
+          }}
+        >
+          <Logo />
+        </Flex>
+        <Text pt={0} fontSize={"sm"} textAlign={"center"}>
+          © 2022.8.11 부터 구축된 블로그입니다.
+        </Text>
+      </Box>
+    </Box>
+  );
+};
 export default Footer;
--- src/components/MainHome/GitHubSocialCard/index.tsx
+++ src/components/MainHome/GitHubSocialCard/index.tsx
@@ -1,128 +1,140 @@
 import {
-    Heading,
-    Avatar,
-    Box,
-    Center,
-    Image,
-    Flex,
-    Text,
-    Stack,
-    Button,
-    useColorModeValue,
-    useToast
-  } from '@chakra-ui/react';
-import axios from 'axios';
-import { useEffect, useState } from 'react';
-import GithubCardbackGround from '../GitHubSocialCard/GithubCardbackGround.png';
+  Heading,
+  Avatar,
+  Box,
+  Center,
+  Image,
+  Flex,
+  Text,
+  Stack,
+  Button,
+  useColorModeValue,
+  useToast,
+} from "@chakra-ui/react";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import GithubCardbackGround from "../GitHubSocialCard/GithubCardbackGround.png";
 
 const GitHubSocialCard = () => {
-    const toast = useToast()
-    const [user, setUser] = useState({
-        name: '',
-        bio: '',
-        followers: '',
-        following: ''
-    });
+  const toast = useToast();
+  const [user, setUser] = useState({
+    name: "",
+    bio: "",
+    followers: "",
+    following: "",
+  });
 
-    const { name, bio, followers, following } = user;
+  const { name, bio, followers, following } = user;
 
-    const getData = async () => {
-        const token = process.env.REACT_APP_TOKEN_VALUE
-        const config = {
-            headers: {
-                Authorization: `Bearer ${token}`
-            }
-        };
-        try {
-            //Successful response
-            const response = await axios.get('https://api.github.com/user',config);
-            setUser({
-                ...user,
-                name: response.data.name,
-                bio: response.data.bio,
-                followers: response.data.followers,
-                following: response.data.following
-            });
-        } catch (error) {
-            //Failed to respond
-            toast({
-                title: '깃허브 데이터를 가져오지 못 했습니다!',
-                position: 'top',
-                status: 'error',
-                isClosable: true,
-            });
-        }
+  const getData = async () => {
+    const token = process.env.REACT_APP_TOKEN_VALUE;
+    const config = {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
     };
+    try {
+      //Successful response
+      const response = await axios.get("https://api.github.com/user", config);
+      setUser({
+        ...user,
+        name: response.data.name,
+        bio: response.data.bio,
+        followers: response.data.followers,
+        following: response.data.following,
+      });
+    } catch (error) {
+      //Failed to respond
+      toast({
+        title: "깃허브 데이터를 가져오지 못 했습니다!",
+        position: "top",
+        status: "error",
+        isClosable: true,
+      });
+    }
+  };
 
-    const githubAddress = () => {
-        window.open('https://github.com/gkseogus');
-    };
+  const githubAddress = () => {
+    window.open("https://github.com/gkseogus");
+  };
 
-    useEffect(() => {
-        getData();
-    }, []);
+  useEffect(() => {
+    getData();
+  }, []);
 
-    return (
-        <Center py={6}>
-            <Box
-                maxW={'400px'}
-                w={'full'}
-                bg={useColorModeValue('black', 'black')}
-                boxShadow={'2xl'}
-                rounded={'md'}
-                overflow={'hidden'}>
-                <Image
-                h={'120px'}
-                w={'full'}
-                src={GithubCardbackGround}
-                objectFit={'cover'}
-                />
-                <Flex justify={'center'} mt={-12}>
-                <Avatar
-                    size={'xl'}
-                    src={'https://avatars.githubusercontent.com/u/76561461?v=4'}
-                    css={{
-                    border: '2px solid white',
-                    }}
-                />
-                </Flex>
-                <Box p={6}>
-                <Stack spacing={5} align={'center'} mb={5}>
-                    <Heading fontSize={'2xl'} fontWeight={500} fontFamily={'body'} color={'white'}>
-                        {name}
-                    </Heading>
-                    <Text color={'white'}>{bio}</Text>
-                </Stack>
-                <Stack direction={'row'} justify={'center'} spacing={6}>
-                    <Stack spacing={0} align={'center'}>
-                    <Text fontWeight={600} color={'white'}>{followers}</Text>
-                    <Text fontSize={'sm'} color={'white'}>
-                        Followers
-                    </Text>
-                    </Stack>
-                    <Stack spacing={0} align={'center'}>
-                    <Text fontWeight={600} color={'white'}>{following}</Text>
-                    <Text fontSize={'sm'} color={'white'}>
-                        Following
-                    </Text>
-                    </Stack>
-                </Stack>
-                <Button
-                    w={'full'}
-                    mt={8}
-                    bg={useColorModeValue('#151f21', 'gray.900')}
-                    color={'white'}
-                    rounded={'md'}
-                    _hover={{
-                    transform: 'translateY(-2px)',
-                    boxShadow: 'lg',
-                    }} onClick={githubAddress}>
-                    Link
-                </Button>
-                </Box>
-            </Box>
-        </Center>
-    );
-}
+  return (
+    <Center py={6}>
+      <Box
+        maxW={"400px"}
+        w={"full"}
+        bg={useColorModeValue("black", "black")}
+        boxShadow={"2xl"}
+        rounded={"md"}
+        overflow={"hidden"}
+      >
+        <Image
+          h={"120px"}
+          w={"full"}
+          src={GithubCardbackGround}
+          objectFit={"cover"}
+        />
+        <Flex justify={"center"} mt={-12}>
+          <Avatar
+            size={"xl"}
+            src={"https://avatars.githubusercontent.com/u/76561461?v=4"}
+            css={{
+              border: "2px solid white",
+            }}
+          />
+        </Flex>
+        <Box p={6}>
+          <Stack spacing={5} align={"center"} mb={5}>
+            <Heading
+              fontSize={"2xl"}
+              fontWeight={500}
+              fontFamily={"body"}
+              color={"white"}
+            >
+              {name}
+            </Heading>
+            <Text color={"white"}>{bio}</Text>
+          </Stack>
+          <Stack direction={"row"} justify={"center"} spacing={6}>
+            <Stack spacing={0} align={"center"}>
+              <Text fontWeight={600} color={"white"}>
+                {followers}
+              </Text>
+              <Text fontSize={"sm"} color={"white"}>
+                Followers
+              </Text>
+            </Stack>
+            <Stack spacing={0} align={"center"}>
+              <Text fontWeight={600} color={"white"}>
+                {following}
+              </Text>
+              <Text fontSize={"sm"} color={"white"}>
+                Following
+              </Text>
+            </Stack>
+          </Stack>
+          <Button
+            w={"full"}
+            mt={8}
+            bg={useColorModeValue("#151f21", "gray.900")}
+            color={"white"}
+            rounded={"md"}
+            _hover={{
+              transform: "translateY(-2px)",
+              boxShadow: "lg",
+            }}
+            onClick={githubAddress}
+          >
+            Link
+          </Button>
+        </Box>
+      </Box>
+    </Center>
+  );
+};
 
 export default GitHubSocialCard;
