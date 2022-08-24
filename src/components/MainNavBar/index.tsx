9219b7b46afc4486d9d1884d7ea4f8f2a9eb0140
gkseogus <fbznffldj998@naver.com>
2022-08-24 10:47:16 +0900

    220824_코드 정리

--- src/components/MainNavBar/index.tsx
+++ src/components/MainNavBar/index.tsx
@@ -79,103 +79,103 @@ const ColorModeToggle = (props: ButtonProps) => {
   );
 };
 
-export default function MainNavBar(_children: any) {
+const MainNavBar = (_children: any) => {
   const { isOpen, onOpen, onClose } = useDisclosure();
 
   return (
-    <div>
-      <Box
-        bg={useColorModeValue("black", "black")}
-        px={4}
-        fontSize={"20px"}
-        fontWeight={600}
-      >
-        <Flex h={32} alignItems={"center"} justifyContent={"space-between"}>
-          <IconButton
-            size={"md"}
-            icon={isOpen ? <CloseIcon /> : <HamburgerIcon />}
-            aria-label={"Open Menu"}
-            display={{ md: "none" }}
-            onClick={isOpen ? onClose : onOpen}
-          />
-          <HStack spacing={8} alignItems={"center"}>
-            <NavBarLogo src={HdhLogo} />
-            <HStack
-              as={"nav"}
-              spacing={4}
-              display={{ base: "none", md: "flex" }}
-              color="white"
-            >
-              {Links.map((link) => (
-                <NavLink key={link} href={link}>
-                  {link}
-                </NavLink>
-              ))}
-            </HStack>
+    <Box
+      bg={useColorModeValue("black", "black")}
+      px={4}
+      fontSize={"20px"}
+      fontWeight={600}
+    >
+      <Flex h={32} alignItems={"center"} justifyContent={"space-between"}>
+        <IconButton
+          size={"md"}
+          icon={isOpen ? <CloseIcon /> : <HamburgerIcon />}
+          aria-label={"Open Menu"}
+          display={{ md: "none" }}
+          onClick={isOpen ? onClose : onOpen}
+        />
+        <HStack spacing={8} alignItems={"center"}>
+          <NavBarLogo src={HdhLogo} />
+          <HStack
+            as={"nav"}
+            spacing={4}
+            display={{ base: "none", md: "flex" }}
+            color="white"
+          >
+            {Links.map((link) => (
+              <NavLink key={link} href={link}>
+                {link}
+              </NavLink>
+            ))}
           </HStack>
-          <Flex alignItems={"center"}>
-            <ColorModeToggle />
-            <Menu>
-              <MenuButton
-                as={Button}
-                rounded={"full"}
-                variant={"link"}
-                cursor={"pointer"}
-                minW={0}
+        </HStack>
+        <Flex alignItems={"center"}>
+          <ColorModeToggle />
+          <Menu>
+            <MenuButton
+              as={Button}
+              rounded={"full"}
+              variant={"link"}
+              cursor={"pointer"}
+              minW={0}
+            >
+              <Avatar size={"md"} src={AvatarImg} bg="black" />
+            </MenuButton>
+            <MenuList color="black">
+              <MenuItem
+                _hover={{
+                  background: "white",
+                  color: "red",
+                }}
+                as="a"
+                href="https://www.instagram.com/dae_hyeon00/"
               >
-                <Avatar size={"md"} src={AvatarImg} bg="black" />
-              </MenuButton>
-              <MenuList color="black">
-                <MenuItem
-                  _hover={{
-                    background: "white",
-                    color: "red",
-                  }}
-                  as="a"
-                  href="https://www.instagram.com/dae_hyeon00/"
-                >
-                  <AvatarMenuListLogo src={InstartgramLogo} />
-                  Instargram
-                </MenuItem>
-                <MenuItem
-                  _hover={{
-                    background: "white",
-                    color: "red",
-                  }}
-                  as="a"
-                  href="https://careerly.co.kr/profiles/496283?from=newsfeed&location=gnb"
-                >
-                  <AvatarMenuListLogo src={CareerlyLogo} />
-                  Careerly
-                </MenuItem>
-                <MenuDivider />
-                <MenuItem
-                  _hover={{
-                    background: "white",
-                    color: "red",
-                  }}
-                  as="a"
-                  href="https://github.com/gkseogus"
-                >
-                  <AvatarMenuListLogo src={GithubLogo} />
-                  Github
-                </MenuItem>
-              </MenuList>
-            </Menu>
-          </Flex>
+                <AvatarMenuListLogo src={InstartgramLogo} />
+                Instargram
+              </MenuItem>
+              <MenuItem
+                _hover={{
+                  background: "white",
+                  color: "red",
+                }}
+                as="a"
+                href="https://careerly.co.kr/profiles/496283?from=newsfeed&location=gnb"
+              >
+                <AvatarMenuListLogo src={CareerlyLogo} />
+                Careerly
+              </MenuItem>
+              <MenuDivider />
+              <MenuItem
+                _hover={{
+                  background: "white",
+                  color: "red",
+                }}
+                as="a"
+                href="https://github.com/gkseogus"
+              >
+                <AvatarMenuListLogo src={GithubLogo} />
+                Github
+              </MenuItem>
+            </MenuList>
+          </Menu>
         </Flex>
-        {isOpen ? (
-          <Box pb={4} display={{ md: "none" }}>
-            <Stack as={"nav"} spacing={4}>
-              {Links.map((link) => (
-                <NavLink key={link} href={link}>
-                  {link}
-                </NavLink>
-              ))}
-            </Stack>
-          </Box>
-        ) : null}
-      </Box>
-    </div>
+      </Flex>
+      {isOpen ? (
+        <Box pb={4} display={{ md: "none" }}>
+          <Stack as={"nav"} spacing={4}>
+            {Links.map((link) => (
+              <NavLink key={link} href={link}>
+                {link}
+              </NavLink>
+            ))}
+          </Stack>
+        </Box>
+      ) : null}
+    </Box>
   );
-}
+};
+
+export default MainNavBar;
