c7a961ad15d8cf83bb4be7138a36841dec0eea01
gkseogus <fbznffldj998@naver.com>
2022-08-24 12:02:21 +0900

    220824_add to scroll to top button

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
-import AllRoutesApp from './routesAll';
-import { ChakraProvider } from '@chakra-ui/react'
-import MainNavBar from './components/MainNavBar';
-import Footer from './components/Footer';
+import AllRoutesApp from "./routesAll";
+import { ChakraProvider } from "@chakra-ui/react";
+import MainNavBar from "./components/MainNavBar";
+import Footer from "./components/Footer";
+import ScrollToTopButton from "./components/ScrollToTopButton";
 
-console.log(`%c
+console.log(
+  `%c
 ██╗  ██╗███████╗██╗     ██╗      ██████╗ 
 ██║  ██║██╔════╝██║     ██║     ██╔═══██╗
 ███████║█████╗  ██║     ██║     ██║   ██║
@@ -16,18 +18,21 @@ console.log(`%c
 ██╔══██║██║  ██║██╔══██║    ██╔══██╗██║     ██║   ██║██║   ██║
 ██║  ██║██████╔╝██║  ██║    ██████╔╝███████╗╚██████╔╝╚██████╔╝
 ╚═╝  ╚═╝╚═════╝ ╚═╝  ╚═╝    ╚═════╝ ╚══════╝ ╚═════╝  ╚═════╝ 
-`,'background:white; color:blue;');
+`,
+  "background:white; color:blue;"
+);
 
 const App = () => {
   return (
     <div className="App">
       <ChakraProvider>
         <MainNavBar />
+        <ScrollToTopButton />
         <AllRoutesApp />
         <Footer />
       </ChakraProvider>
     </div>
   );
-}
+};
 
 export default App;
