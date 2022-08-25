b96be06c1b40f94aa7f8dd2fbbcca7d60d8c59b8
gkseogus <fbzbffldj@gmail.com>
2022-08-25 16:21:48 +0900

    220825_contents data

---
+++ src/components/ProjectPage/FirstContentsItem/verData.ts
@@ -0,0 +1,37 @@
+import GitLogo from "./IMG/GitLogo.png";
+import GitHubLogo from "./IMG/GitHubLogo.png";
+import ForkLogo from "./IMG/ForkLogo.png";
+import JiraLogo from "./IMG/JiraLogo.png";
+
+const verData = [
+  {
+    name: "Git",
+    description: "#a8edea → #fed6e3",
+    css: "white",
+    height: 200,
+    langthImg: GitLogo,
+  },
+  {
+    name: "Git hub",
+    description: "#f5f7fa → #c3cfe2",
+    css: "white",
+    height: 400,
+    langthImg: GitHubLogo,
+  },
+  {
+    name: "Fork",
+    description: "#f5f7fa → #c3cfe2",
+    css: "white",
+    height: 400,
+    langthImg: ForkLogo,
+  },
+  {
+    name: "Jira",
+    description: "#f5f7fa → #c3cfe2",
+    css: "white",
+    height: 400,
+    langthImg: JiraLogo,
+  },
+];
+
+export default verData;
