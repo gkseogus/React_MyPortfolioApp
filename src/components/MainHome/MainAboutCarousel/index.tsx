61ebe31833031b0077fa42d6eea38aa8d2cbb645
gkseogus <fbzbffldj@gmail.com>
2022-08-30 16:35:11 +0900

    220830_main about carousel 이미지 오류 수정

--- src/components/MainHome/MainAboutCarousel/index.tsx
+++ src/components/MainHome/MainAboutCarousel/index.tsx
@@ -74,40 +74,48 @@ export default class MainAboutCarousel extends Component {
     return (
       <Contain>
         <Slider {...settings}>
-          <ImgContain>
-            <KoTextContain>
-              안녕하세요. <br /> 프론트 엔지니어 한대현입니다. <br />
-            </KoTextContain>
-            <EnTextContain>
-              ━ <br /> Welcome to <br /> Dae Hyeon's portfolio blog
-            </EnTextContain>
-          </ImgContain>
-          <ImgContain>
-            <KoTextContain>
-              주로 React & Redux 를 <br />
-              다루며 아래와 같은 기술도 다룹니다. <br />
-            </KoTextContain>
-            <EnTextContain>
-              ━ <br /> JavaScript, TypeScript, python, ...
-            </EnTextContain>
-          </ImgContain>
-          <ImgContain>
-            <KoTextContain>
-              배우는 것을 좋아하며 <br /> 만드는 것에 흥미를 느낍니다. <br />
-            </KoTextContain>
-            <EnTextContain>
-              ━ <br /> I like to learn and <br /> I am interested in making.
-            </EnTextContain>
-          </ImgContain>
-          <ImgContain>
-            <KoTextContain>
-              저의 연락처 입니다. <br />
-            </KoTextContain>
-            <EnTextContain>
-              ━ <br /> call : 010-2246-6787 <br /> e-mail :
-              fbznffldj998@naver.com
-            </EnTextContain>
-          </ImgContain>
+          <div>
+            <ImgContain>
+              <KoTextContain>
+                안녕하세요. <br /> 프론트 엔지니어 한대현입니다. <br />
+              </KoTextContain>
+              <EnTextContain>
+                ━ <br /> Welcome to <br /> Dae Hyeon's portfolio blog
+              </EnTextContain>
+            </ImgContain>
+          </div>
+          <div>
+            <ImgContain>
+              <KoTextContain>
+                주로 React & Redux 를 <br />
+                다루며 아래와 같은 기술도 다룹니다. <br />
+              </KoTextContain>
+              <EnTextContain>
+                ━ <br /> JavaScript, TypeScript, python, ...
+              </EnTextContain>
+            </ImgContain>
+          </div>
+          <div>
+            <ImgContain>
+              <KoTextContain>
+                배우는 것을 좋아하며 <br /> 만드는 것에 흥미를 느낍니다. <br />
+              </KoTextContain>
+              <EnTextContain>
+                ━ <br /> I like to learn and <br /> I am interested in making.
+              </EnTextContain>
+            </ImgContain>
+          </div>
+          <div>
+            <ImgContain>
+              <KoTextContain>
+                저의 연락처 입니다. <br />
+              </KoTextContain>
+              <EnTextContain>
+                ━ <br /> call : 010-2246-6787 <br /> e-mail :
+                fbznffldj998@naver.com
+              </EnTextContain>
+            </ImgContain>
+          </div>
         </Slider>
       </Contain>
     );
