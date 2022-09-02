6b949affce0a21154aaeacc48bc2a4f3e9108e44
gkseogus <fbzbffldj@gmail.com>
2022-09-02 13:21:46 +0900

    220902_mo img 삭제 및 이미지 이름 변경

--- src/components/ProjectPage/SkillCardsItem/ProgressData/frontLabelData.ts
+++ src/components/ProjectPage/SkillCardsItem/ProgressData/frontLabelData.ts
@@ -1,19 +1,28 @@
+import javaScriptLogo from "../LogoImg/javaScriptLogo.svg";
+import typeScriptLogo from "../LogoImg/typeScriptLogo.svg";
+import reactLogo from "../LogoImg/reactLogo.png";
+import reduxLogo from "../LogoImg/reduxLogo.png";
+
 const frontLabelData = [
   {
     title: "Java Script",
     persent: "30%",
+    logoImg: javaScriptLogo,
   },
   {
     title: "Type Script",
     persent: "30%",
+    logoImg: typeScriptLogo,
   },
   {
     title: "React",
     persent: "35%",
+    logoImg: reactLogo,
   },
   {
     title: "Redux",
     persent: "25%",
+    logoImg: reduxLogo,
   },
 ];
 
--- src/components/ProjectPage/SkillCardsItem/ProgressData/verLabelData.ts
+++ src/components/ProjectPage/SkillCardsItem/ProgressData/verLabelData.ts
@@ -1,19 +1,28 @@
+import gitLogo from "../LogoImg/gitLogo.png";
+import gitHubLogo from "../LogoImg/gitHubLogo.png";
+import forkLogo from "../LogoImg/forkLogo.png";
+import jiraLogo from "../LogoImg/jiraLogo.png";
+
 const verLabelData = [
   {
     title: "Git",
     persent: "45%",
+    logoImg: gitLogo,
   },
   {
     title: "Git hub",
     persent: "55%",
+    logoImg: gitHubLogo,
   },
   {
     title: "Fork",
     persent: "40%",
+    logoImg: forkLogo,
   },
   {
     title: "Jira",
     persent: "35%",
+    logoImg: jiraLogo,
   },
 ];
 
