039b20447f34ff6b09b0d30ff706cc7fdc3c2a35
gkseogus <fbzbffldj@gmail.com>
2022-09-15 18:35:46 +0900

    220915_add to data

--- src/components/ProjectPage/SkillCardsItem/ProgressData/backLabelData.ts
+++ src/components/ProjectPage/SkillCardsItem/ProgressData/backLabelData.ts
@@ -1,22 +1,40 @@
 import pythonLogo from "../LogoImg/pythonLogo.png";
 import awsLogo from "../LogoImg/awsLogo.png";
 import googleApiLogo from "../LogoImg/googleApiLogo.png";
+import nodeJsLogo from "../LogoImg/nodeJsLogo.png";
+import javaLogo from "../LogoImg/javaLogo.png";
+import mySqlLogo from "../LogoImg/mySqlLogo.png";
 
 const backLabelData = [
   {
     title: "Python",
-    persent: "20%",
+    persent: "30%",
     logoImg: pythonLogo,
   },
   {
     title: "AWS",
-    persent: "10%",
+    persent: "35%",
     logoImg: awsLogo,
   },
   {
     title: "Google api",
-    persent: "20%",
+    persent: "25%",
     logoImg: googleApiLogo,
+  },
+  {
+    title: "Node js",
+    persent: "25%",
+    logoImg: nodeJsLogo,
+  },
+  {
+    title: "JAVA",
+    persent: "20%",
+    logoImg: javaLogo,
+  },
+  {
+    title: "My SQL",
+    persent: "10%",
+    logoImg: mySqlLogo,
   },
 ];
 
