b351b1aba4a88ac38c143ea1cb44079e3feef2fb
gkseogus <fbzbffldj@gmail.com>
2022-09-27 16:51:03 +0900

    220927_add to deploy label data

---
+++ src/components/ProjectPage/SkillCardsItem/ProgressData/deployLabelData.ts
@@ -0,0 +1,17 @@
+import awsLogo from "../LogoImg/awsLogo.svg";
+import googleApiLogo from "../LogoImg/googleApiLogo.svg";
+
+const deployLabelData = [
+  {
+    title: "AWS",
+    persent: "35%",
+    logoImg: awsLogo,
+  },
+  {
+    title: "Google",
+    persent: "25%",
+    logoImg: googleApiLogo,
+  },
+];
+
+export default deployLabelData;
