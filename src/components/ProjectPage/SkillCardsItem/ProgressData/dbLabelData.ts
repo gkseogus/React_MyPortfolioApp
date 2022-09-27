da17c1fcb5370664920b50e27ba93273fec0db69
gkseogus <fbzbffldj@gmail.com>
2022-09-27 16:50:42 +0900

    220927_add to db label data

---
+++ src/components/ProjectPage/SkillCardsItem/ProgressData/dbLabelData.ts
@@ -0,0 +1,10 @@
+import mySqlLogo from "../LogoImg/mySqlLogo.svg";
+
+const dbLabelData = [
+  {
+    title: "My SQL",
+    persent: "25%",
+    logoImg: mySqlLogo,
+  },
+];
+export default dbLabelData;
