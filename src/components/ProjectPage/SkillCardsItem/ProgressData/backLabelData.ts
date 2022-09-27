78fe0838e8b46ac37b8491f84785204384833df7
gkseogus <fbzbffldj@gmail.com>
2022-09-27 16:49:33 +0900

    220927_fix to data 변경

--- src/components/ProjectPage/SkillCardsItem/ProgressData/backLabelData.ts
+++ src/components/ProjectPage/SkillCardsItem/ProgressData/backLabelData.ts
@@ -1,9 +1,6 @@
 import pythonLogo from "../LogoImg/pythonLogo.svg";
-import awsLogo from "../LogoImg/awsLogo.svg";
-import googleApiLogo from "../LogoImg/googleApiLogo.svg";
 import nodeJsLogo from "../LogoImg/nodeJsLogo.svg";
 import javaLogo from "../LogoImg/javaLogo.svg";
-import mySqlLogo from "../LogoImg/mySqlLogo.svg";
 
 const backLabelData = [
   {
@@ -12,29 +9,14 @@ const backLabelData = [
     logoImg: pythonLogo,
   },
   {
-    title: "AWS",
-    persent: "35%",
-    logoImg: awsLogo,
-  },
-  {
-    title: "Google api",
-    persent: "25%",
-    logoImg: googleApiLogo,
-  },
-  {
     title: "Node js",
-    persent: "25%",
+    persent: "20%",
     logoImg: nodeJsLogo,
   },
   {
     title: "JAVA",
     persent: "20%",
     logoImg: javaLogo,
-  },
-  {
-    title: "My SQL",
-    persent: "10%",
-    logoImg: mySqlLogo,
   },
 ];
 
