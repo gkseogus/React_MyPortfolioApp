773ead90b6162952225b22429a3c255d62bbd2b9
gkseogus <fbzbffldj@gmail.com>
2022-09-29 15:35:40 +0900

    220929_add to bbs path

--- src/routesAll.tsx
+++ src/routesAll.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { v4 as uuidv4 } from "uuid";
+import BbsPage from "./components/BbsPage";
 import CareerPage from "./components/CareerPage";
 import EmailPage from "./components/EmailPage";
 import MainHome from "./components/MainHome";
@@ -57,6 +58,15 @@ const AllRoutesApp = () => (
           />
         );
       })}
+      <Route
+        path="/Bbs"
+        element={
+          <div>
+            <MainNavBar />
+            <BbsPage />
+          </div>
+        }
+      />
     </Routes>
   </Router>
 );
