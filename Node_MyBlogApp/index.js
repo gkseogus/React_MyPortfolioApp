27672091249f406d562056c9cfe48b8b1ff9f6be
gkseogus <fbzbffldj@gmail.com>
2022-09-29 17:35:01 +0900

    220929_add to node server

---
+++ Node_MyBlogApp/index.js
@@ -0,0 +1,26 @@
+const cors = require("cors");
+const express = require("express");
+const app = express();
+const mysql = require("mysql");
+const PORT = process.env.port || 8000;
+
+const db = mysql.createPool({
+  host: "localhost",
+  user: "root",
+  password: "gkseogus!",
+  database: "gkseogusblog",
+});
+
+app.use(cors({ credentials: true, origin: true }));
+
+// api
+app.get("/list", (req, res) => {
+  const sqlQuery = "SELECT *FROM BOARD;";
+  db.query(sqlQuery, (err, result) => {
+    res.send(result);
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`running on port ${PORT}`);
+});
