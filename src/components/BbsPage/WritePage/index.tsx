58a529a7f73dbd2a4abc6f827837ceaecd40f99f
gkseogus <fbzbffldj@gmail.com>
2022-09-30 16:12:33 +0900

    220930_add to write page

--- src/components/BbsPage/WritePage/index.tsx
+++ src/components/BbsPage/WritePage/index.tsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import styled from "styled-components";
 import { Button, Input, Textarea } from "@chakra-ui/react";
 import BbsPage from "..";
+import axios from "axios";
 
 const Contain = styled.div`
   display: flex;
@@ -17,11 +18,15 @@ const SubContentsContain = styled.div`
   padding: 3%;
 `;
 
-const TextContain = styled.p`
+const TRtextContain = styled.div`
+  width: 25%;
+`;
+
+const ContentText = styled.p`
   font-size: 24px;
   font-weight: 600px;
   font-family: "Kanit", sans-serif;
-  text-align: center;
+  text-align: left;
 `;
 
 const BtnContain = styled.div`
@@ -30,9 +35,34 @@ const BtnContain = styled.div`
 
 const WritePage = () => {
   const [write, setWrite] = useState(false);
+  const [writeData, setWriteData] = useState({
+    title: "",
+    content: "",
+    register: "",
+  });
 
   const changeWritePage = () => {
     setWrite(!write);
+  };
+
+  const writePost = async () => {
+    try {
+      //Successful response
+      await axios.post("http://localhost:8000/api/insert", {
+        title: writeData.title,
+        content: writeData.content,
+        register: writeData.register,
+      });
+      setWrite(!write);
+    } catch (error) {
+      //Failed to respond
+      console.log("write error", error);
+    }
+  };
+
+  //input에 입력될 때마다 writeData state값 변경되게 하는 함수
+  const handleChange = (e: any) => {
+    setWriteData({ ...writeData, [e.target.name]: e.target.value });
   };
 
   return (
@@ -43,19 +73,38 @@ const WritePage = () => {
         <Contain>
           <ContentsContain>
             <SubContentsContain>
-              <TextContain>제목</TextContain>
-              <Input placeholder="title" />
-            </SubContentsContain>
-            <SubContentsContain>
-              <TextContain>내용</TextContain>
-              <Textarea placeholder="content" size={"lg"} variant="outline" />
+              <TRtextContain>
+                <ContentText>제목</ContentText>
+                <Input
+                  type={"text"}
+                  name="title"
+                  placeholder="title"
+                  maxLength={10}
+                  marginBottom={5}
+                  onChange={handleChange}
+                />
+                <ContentText>작성자</ContentText>
+                <Input
+                  type={"text"}
+                  name="register"
+                  placeholder="register"
+                  maxLength={10}
+                  marginBottom={5}
+                  onChange={handleChange}
+                />
+              </TRtextContain>
+              <ContentText>내용</ContentText>
+              <Textarea
+                as={"textarea"}
+                name="content"
+                placeholder="content"
+                size={"lg"}
+                variant="outline"
+                onChange={handleChange}
+              />
             </SubContentsContain>
             <BtnContain>
-              <Button
-                colorScheme="teal"
-                variant="ghost"
-                onClick={changeWritePage}
-              >
+              <Button colorScheme="teal" variant="ghost" onClick={writePost}>
                 작성완료
               </Button>
               <Button
