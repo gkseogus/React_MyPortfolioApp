d3b61ca0d1ed2cc49f9bb7a785a702e426e5757f
gkseogus <fbzbffldj@gmail.com>
2022-10-21 10:26:57 +0900

    221021_add to mariadb label

--- src/components/ProjectPage/SkillCardsItem/ProgressData/dbLabelData.ts
+++ src/components/ProjectPage/SkillCardsItem/ProgressData/dbLabelData.ts
@@ -1,10 +1,16 @@
 import mySqlLogo from "../LogoImg/mySqlLogo.svg";
+import mariaDbLogo from "../LogoImg/mariaDbLogo.svg";
 
 const dbLabelData = [
   {
     title: "My SQL",
     persent: "25%",
     logoImg: mySqlLogo,
+  },
+  {
+    title: "MariaDB",
+    persent: "25%",
+    logoImg: mariaDbLogo,
   },
 ];
 export default dbLabelData;
